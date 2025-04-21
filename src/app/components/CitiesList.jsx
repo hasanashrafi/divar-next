47313c9b469d22e81bd79f4a718cd14ca8fdc9f9
Hasan Ashrafi <yasashrafi@gmail.com>
2025-04-21 12:33:35 +0330

    update cities list

--- src/app/components/CitiesList.jsx
+++ src/app/components/CitiesList.jsx
@@ -2,17 +2,15 @@ import cities from "@/utils/citiesList";
 import Link from "next/link";
 
 export default function CitiesList() {
-
-
     return (
-        <div className="bg-white rounded-lg shadow p-6">
-            <h2 className="text-lg text-gray-600  mb-4">شهرهای پربازدید</h2>
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+        <div className="bg-white rounded-lg shadow-md p-6">
+            <h2 className="text-lg text-gray-600 mb-4">شهرهای پربازدید</h2>
+            <div className="flex flex-wrap gap-2">
                 {cities.map((city) => (
                     <Link
                         key={city}
                         href="/"
-                        className="text-gray-700 hover:text-gray-900 hover:bg-gray-300 p-2 rounded-md ease-in-out transition-all text-center"
+                        className="px-4 py-2 bg-gray-50 text-gray-700 rounded-full text-sm transition-all duration-200 hover:bg-red-500 hover:text-white"
                     >
                         {city}
                     </Link>
