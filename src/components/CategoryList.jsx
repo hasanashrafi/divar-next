8ec1dce04a84d07e6286ee5c35b8ffca9a69f146
Hasan Ashrafi <yasashrafi@gmail.com>
2025-04-23 19:30:44 +0330

    Add CategoryList component to display categories with links and images

---
+++ src/components/CategoryList.jsx
@@ -0,0 +1,33 @@
+import categories from "@/utils/categoriesList";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function CategoryList() {
+    return (
+        <div className="bg-white shadow-md rounded-lg p-4">
+            <h2 className="text-gray-600 font-open-sans text-lg mb-4">
+                دسته بندی ها
+            </h2>
+            <div className="flex flex-wrap justify-start md:flex mx-auto  lg:flex-col items-center gap-4">
+                {categories.map((category) => (
+                    <Link
+                        key={category.id}
+                        href={`/categories/${category.name}`}
+                        className="flex p-1.5 lg:w-full md:w-40 overflow-hidden items-center gap-x-1  rounded-lg transition-all hover:bg-gray-100 duration-200 hover:transform hover:-translate-y-1 hover:shadow-md cursor-pointer"
+                    >
+                        <Image
+                            src={category.image_url}
+                            alt={category.name}
+                            width={48}
+                            height={48}
+                            className="mb-2"
+                        />
+                        <span className="hidden md:block text-sm font-medium text-gray-700 text-center">
+                            {category.name}
+                        </span>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
+}
